b22a78518d9f2f9e1fbb860b818e2e4d711ff8d2
ModernCrusader97 <whdtjr970717@gmail.com>
2026-03-23 21:48:26 +0900

    Feature: DB에 태이블& 더미데이터 추가, 마이페이지를 위한 ReservaionController와 Service 추가

---
+++ src/main/java/LiveTicket/Reservation/ReservationService.java
@@ -0,0 +1,35 @@
+package LiveTicket.Reservation;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import LiveTicket.SecSql;
+import LiveTicket.DBUtil;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+public class ReservationService {
+	private Connection conn;
+	
+	
+	public ReservationService(Connection conn) {
+        this.conn = conn;
+    }
+	public  List<Map<String, Object>> getMyReservations(int loginedMemberId)
+	{
+
+
+		
+	}
+
+}
